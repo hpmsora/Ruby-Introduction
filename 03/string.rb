32b15469571deaef75c0b4bcbde6773c6175f1aa
Burning Marshmallow <marshmallow@dhcp-rhodes-1159.redrover.cornell.edu>
2017-02-13 12:36:51 -0500

    13 February 2017

---
+++ 03/string.rb
@@ -0,0 +1,19 @@
+words = <<HERE
+Now is the time
+for all food
+people to come
+to the aid
+of their party.
+HERE
+print(words)
+
+first,last = "John,Smith".split(/,/)
+
+puts "John,Smith".split(/,/)
+
+print(first + "\n")
+print(last + "\n")
+
+print(first + last + "\n")
+
+print(first.squeeze + last + "\n")
---
+++ 04/math.rb
@@ -0,0 +1,9 @@
+puts 0x10
+
+puts 2.2 - 0.008
+
+puts -23.abs
+
+puts 100.to_s
+
+puts 12.modulo(8)
